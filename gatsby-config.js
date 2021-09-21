e1e3af20f632acf9a0bb4fa10b8d6b8951291c59
orioncohen <orion@lbl.gov>
2021-09-21 10:50:17 -0700

    iupdated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Math Bootcamp',
+    author: 'Orion Cohen',
+    description: 'Content and information for a chemistry math bootcamp.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
